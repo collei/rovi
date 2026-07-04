e6f0af5f2dbdde48f26e9a4963095975ebeb567b
Collei <collei@collei.com.br>
2026-07-04 20:55:09 -0300

    renamed relation class

---
+++ src/Rovi/Repository/Relations/HasMany.php
@@ -0,0 +1,27 @@
+<?php
+namespace Rovi\Repository\Relations;
+
+use InvalidArgumentException;
+use LogicException;
+use Rovi\Repository\Model;
+use Rovi\Query\Builder;
+use Rovi\Connections\Connection;
+use Collei\Collections\Collection;
+
+/**
+ * One-to-many relationship.
+ */
+class HasMany extends Relation
+{
+    /**
+     * Retrieves the relation query.
+     * 
+     * @return Rovi\Query\Builder
+     */
+    public function query()
+    {
+        return $this->connection()->getBuilder()
+                    ->table($this->rightTable())
+                    ->where($this->foreignKey(true), '=', $this->left()->getKey());
+    }
+}
