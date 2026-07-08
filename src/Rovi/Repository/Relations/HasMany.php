bdac355695bbcd5630185484c114ea6f84f3712b
Collei <collei@collei.com.br>
2026-07-08 15:50:26 -0300

    added specialized constructor to fix wrongings

--- src/Rovi/Repository/Relations/HasMany.php
+++ src/Rovi/Repository/Relations/HasMany.php
@@ -17,6 +17,27 @@ class HasMany extends Relation
      * @var bool
      */
     private $queryCalled = false;
+    
+    /**
+     * Instantiator.
+     * 
+     * @param Rovi\Repository\Model $left
+     * @param string $right
+     * @param string|null $foreignKey
+     * @param string|null $localKey
+     * @throws InvalidArgumentException when the first or second arguments aren't classnames extending Model.
+     * @throws LogicException when both models do not use the same database connection.
+     */
+    public function __construct(Model $left, string $rightClass, ?string $foreignKey = null, ?string $localKey = null)
+    {
+        parent::__construct($left, $rightClass);
+
+        if (! empty($localKey)) {
+            $this->localKey = $localKey;
+        }
+        
+        $this->foreignKey = $foreignKey ?: $this->leftClass(true, true).'_id';
+    }
 
     /**
      * Retrieves the relation query.
