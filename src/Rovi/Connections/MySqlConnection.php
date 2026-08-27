f26902d8e26f22f8f58d3809324689a437bb22f1
Collei <collei@collei.com.br>
2026-08-26 21:29:02 -0300

    fixed missing import, fixed wrong db type

--- src/Rovi/Connections/MySqlConnection.php
+++ src/Rovi/Connections/MySqlConnection.php
@@ -3,6 +3,7 @@ namespace Rovi\Connections;
 
 use Rovi\Query\Grammars\MySqlGrammar;
 use Rovi\Query\Grammars\MariaDbGrammar;
+use PDO;
 
 /**
  * SqlServer connection.
@@ -19,7 +20,7 @@ class MySqlConnection extends Connection
 	 */
 	public function __construct($dsn = '', string $database = '', ?string $username = null, ?string $password = null)
     {
-        parent::__construct('sqlite', $dsn, $database, $username, $password);
+        parent::__construct('mysql', $dsn, $database, $username, $password);
     }
 
     /**
